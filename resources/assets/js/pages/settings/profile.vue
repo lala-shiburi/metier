61e651bae60c43905bde31099b51d50d38051a20
Jesray Garciano <jesray.garciano@gmail.com>
2018-06-13 15:23:41 +0800

    Adding vueJS and SASS compilation

---
+++ resources/assets/js/pages/settings/profile.vue
@@ -0,0 +1,78 @@
+<template>
+  <card :title="$t('your_info')">
+    <form @submit.prevent="update" @keydown="form.onKeydown($event)">
+      <alert-success :form="form" :message="$t('info_updated')"/>
+
+      <!-- Name -->
+      <div class="form-group row">
+        <label class="col-md-3 col-form-label text-md-right">{{ $t('name') }}</label>
+        <div class="col-md-7">
+          <div class="form-group">
+            <input placeholder="first name" v-model="form.first_name" :class="{ 'is-invalid': form.errors.has('first_name') }" class="form-control" type="text" name="first_name">
+            <has-error :form="form" field="first_name"/>
+          </div>
+          <div class="form-group">
+            <input placeholder="last name" v-model="form.last_name" :class="{ 'is-invalid': form.errors.has('last_name') }" class="form-control" type="text" name="last_name">
+            <has-error :form="form" field="last_name"/>
+          </div>
+        </div>
+      </div>
+
+      <!-- Email -->
+      <div class="form-group row">
+        <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
+        <div class="col-md-7">
+          <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
+          <has-error :form="form" field="email" />
+        </div>
+      </div>
+
+      <!-- Submit Button -->
+      <div class="form-group row">
+        <div class="col-md-9 ml-md-auto">
+          <v-button :loading="form.busy" type="success">{{ $t('update') }}</v-button>
+        </div>
+      </div>
+    </form>
+  </card>
+</template>
+
+<script>
+import Form from 'vform'
+import { mapGetters } from 'vuex'
+
+export default {
+  scrollToTop: false,
+
+  metaInfo () {
+    return { title: this.$t('settings') }
+  },
+
+  data: () => ({
+    form: new Form({
+      first_name: '',
+      last_name: '',
+      email: ''
+    })
+  }),
+
+  computed: mapGetters({
+    user: 'auth/user'
+  }),
+
+  created () {
+    // Fill the form with user data.
+    this.form.keys().forEach(key => {
+      this.form[key] = this.user[key]
+    })
+  },
+
+  methods: {
+    async update () {
+      const { data } = await this.form.patch('/api/settings/profile')
+
+      this.$store.dispatch('auth/updateUser', { user: data })
+    }
+  }
+}
+</script>
