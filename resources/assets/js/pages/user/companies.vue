02a8d824dcc2615f02f52ac67da1f4e797fa4f3d
Jesray Garciano <jesray.garciano@gmail.com>
2018-07-04 10:04:33 +0800

    Vue pages and controllers

---
+++ resources/assets/js/pages/user/companies.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <company-search-table></company-search-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+
+  computed: {
+    tabs () {
+      return [
+        {
+          icon: 'user',
+          name: this.$t('profile'),
+          route: 'settings.profile'
+        },
+        {
+          icon: 'lock',
+          name: this.$t('password'),
+          route: 'settings.password'
+        }
+      ]
+    }
+  },
+  data : () =>({
+    public_path: location.origin
+  })
+}
+</script>
+
+<style>
+.settings-card .card-body {
+  padding: 0;
+}
+</style>
--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -8,6 +8,7 @@ const NotFound = () => import('~/pages/errors/404').then(m => m.default || m)
 const Home = () => import('~/pages/home').then(m => m.default || m)
 const UserProfile = () => import('~/pages/user/index').then(m => m.default || m)
 const CompanyProfile = () => import('~/pages/company/index').then(m => m.default || m)
+// const UserCompanies = () => import('~/pages/user/companies').then(m => m.default || m)
 const CompanyCreate = () => import('~/pages/company/create').then(m => m.default || m)
 const Settings = () => import('~/pages/settings/index').then(m => m.default || m)
 const SettingsProfile = () => import('~/pages/settings/profile').then(m => m.default || m)
@@ -36,6 +37,7 @@ export default [
 
   // user routes
   { path: '/profile/:id?', name: 'user.profile', component: UserProfile },
+  // { path: '/user/companies', name: 'user.companies', component: UserCompanies },
   
 
   { path: '*', component: NotFound }
