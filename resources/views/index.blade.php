8721df90898255401768ed72c57007fca098a1e2
Jesray Garciano <jesray.garciano@gmail.com>
2018-07-23 16:37:30 +0800

    Adding External CSS to Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,7 +56,11 @@ $polyfills = [
 
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/nucleo-icons.css') }}">
+  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+
 </head>
 <body>
   <div id="app"></div>
