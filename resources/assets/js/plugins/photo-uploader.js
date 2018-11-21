229e19b65bfbd4c5010ce11f6069d2062f1301eb
Jesray Garciano <jesray.garciano@gmail.com>
2018-11-21 10:24:21 +0800

    Views updates on photo manangement and added components

--- resources/assets/js/plugins/photo-uploader.js
+++ resources/assets/js/plugins/photo-uploader.js
@@ -1,16 +1,19 @@
-import axios from 'axios'
+import Form from 'vform'
 
 export default {
-  uploadPhoto : (file) => {
+  uploadPhoto : (file, url, params) => {
     return new Promise((resolve, reject)=>{
           var reader = new FileReader()
           reader.onload = function(event){
-            console.log(event)
-            // axios({
-            //   url: '/api/photo/upload',
-            //   method: 'post',
-            //   params: {}
-            // }).then()
+            var form = new Form({
+              photo_data : event.target.result
+            })
+
+            jQuery.extend(form, params)
+
+            form.post(url || '/api/photo/upload/temp').then(({data})=>{
+              resolve(data)
+            })
           }
           reader.readAsDataURL(file);
       })
