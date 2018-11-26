4fa797fcf91f517dd4ab225b4e6a454f9f107a77
Jesray Garciano <jesray.garciano@gmail.com>
2018-11-26 16:13:49 +0800

    Minor updates

--- resources/assets/js/footer/index.vue
+++ resources/assets/js/footer/index.vue
@@ -1,55 +1,126 @@
 <template>
-  <footer>
+<!-- Footer -->
+<footer class="page-footer font-small indigo">
+
+    <!-- Footer Links -->
     <div class="container">
-      <br>
-      <p class="text-center">
-        <ul class="list-unstyled">
-          <li>
-            <router-link :to="{ name: 'contact-us' }">
-              Contact Us
-            </router-link>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/Jobseed-473898096451976/" target="jobseed-on-facebook">
-              Follow Us On Facebook
-            </a>
-          </li>
-          <li>
+
+      <!-- Grid row-->
+      <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="#!">About us</a>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="#!">Products</a>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="#!">Awards</a>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
             <a href="http://docs.jobseed.net/" target="technical-documentation">
-              Technical Documentation
+               Documentation
             </a>
-          </li>
-        </ul>
-      </p>
-      <p>
-        <!-- <ul class="social_footer_ul">
-          <li>
-            <a href="#">
-              <i class="fa fa-facebook-f"></i>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="#!">Contact</a>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+      <hr class="rgba-white-light" style="margin: 0 15%;">
+
+      <!-- Grid row-->
+      <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+        <!-- Grid column -->
+        <div class="col-md-8 col-12 mt-5">
+          <p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+      <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+      <!-- Grid row-->
+      <div class="row pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-12">
+
+          <div class="mb-5 flex-center">
+
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fa fa-facebook fa-lg white-text mr-4"> </i>
             </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-twitter"></i>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fa fa-twitter fa-lg white-text mr-4"> </i>
             </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-linkedin"></i>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fa fa-google-plus fa-lg white-text mr-4"> </i>
             </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-instagram"></i>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fa fa-linkedin fa-lg white-text mr-4"> </i>
             </a>
-          </li>
-        </ul> -->
-      </p>
-      <p class="text-center">Copyright @ 2018
-        <a href="https://nexseed.net/" target="nexseed-website">Nexseed</a>
-      </p>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fa fa-instagram fa-lg white-text mr-4"> </i>
+            </a>
+            <!--Pinterest-->
+            <a class="pin-ic">
+              <i class="fa fa-pinterest fa-lg white-text"> </i>
+            </a>
+
+          </div>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+
     </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
   </footer>
+  <!-- Footer -->
 </template>
 
 <script>
